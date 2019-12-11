d17aaa024e32d331e76476a260c7e2def4a0c770
Thái Thiên Vũ <thienvu1802@gmail.com>
2019-12-11 19:51:51 +0700

    Add type to return

--- src/api/user/controller.js
+++ src/api/user/controller.js
@@ -1,88 +1,91 @@
-import { success, notFound } from '../../services/response/'
-import { User } from '.'
-import { sign } from '../../services/jwt'
+import { success, notFound } from "../../services/response/";
+import { User } from ".";
+import { sign } from "../../services/jwt";
 
 export const index = ({ querymen: { query, select, cursor } }, res, next) =>
   User.find(query, select, cursor)
-    .then((users) => users.map((user) => user.view()))
+    .then(users => users.map(user => user.view()))
     .then(success(res))
-    .catch(next)
+    .catch(next);
 
 export const show = ({ params }, res, next) =>
   User.findById(params.id)
     .then(notFound(res))
-    .then((user) => user ? user.view() : null)
+    .then(user => (user ? user.view() : null))
     .then(success(res))
-    .catch(next)
+    .catch(next);
 
-export const showMe = ({ user }, res) =>
-  res.json(user.view(true))
+export const showMe = ({ user }, res) => res.json(user.view(true));
 
 export const create = ({ bodymen: { body } }, res, next) =>
   User.create(body)
     .then(user => {
       sign(user.id)
-        .then((token) => ({ token, user: user.view(true) }))
-        .then(success(res, 201))
+        .then(token => ({ token, user: user.view(true) }))
+        .then(success(res, 201));
     })
-    .catch((err) => {
+    .catch(err => {
       /* istanbul ignore else */
-      if (err.name === 'MongoError' && err.code === 11000) {
+      if (err.name === "MongoError" && err.code === 11000) {
         res.status(409).json({
           valid: false,
-          param: 'email',
-          message: 'email already registered'
-        })
+          param: "email",
+          message: "email already registered"
+        });
       } else {
-        next(err)
+        next(err);
       }
-    })
+    });
 
 export const update = ({ bodymen: { body }, params, user }, res, next) =>
-  User.findById(params.id === 'me' ? user.id : params.id)
+  User.findById(params.id === "me" ? user.id : params.id)
     .then(notFound(res))
-    .then((result) => {
-      if (!result) return null
-      const isAdmin = user.role === 'admin'
-      const isSelfUpdate = user.id === result.id
+    .then(result => {
+      if (!result) return null;
+      const isAdmin = user.role === "admin";
+      const isSelfUpdate = user.id === result.id;
       if (!isSelfUpdate && !isAdmin) {
         res.status(401).json({
           valid: false,
-          message: 'You can\'t change other user\'s data'
-        })
-        return null
+          message: "You can't change other user's data"
+        });
+        return null;
       }
-      return result
+      return result;
     })
-    .then((user) => user ? Object.assign(user, body).save() : null)
-    .then((user) => user ? user.view(true) : null)
+    .then(user => (user ? Object.assign(user, body).save() : null))
+    .then(user => (user ? user.view(true) : null))
     .then(success(res))
-    .catch(next)
+    .catch(next);
 
-export const updatePassword = ({ bodymen: { body }, params, user }, res, next) =>
-  User.findById(params.id === 'me' ? user.id : params.id)
+export const updatePassword = (
+  { bodymen: { body }, params, user },
+  res,
+  next
+) =>
+  User.findById(params.id === "me" ? user.id : params.id)
     .then(notFound(res))
-    .then((result) => {
-      if (!result) return null
-      const isSelfUpdate = user.id === result.id
+    .then(result => {
+      if (!result) return null;
+      const isSelfUpdate = user.id === result.id;
       if (!isSelfUpdate) {
         res.status(401).json({
           valid: false,
-          param: 'password',
-          message: 'You can\'t change other user\'s password'
-        })
-        return null
+          param: "password",
+          message: "You can't change other user's password"
+        });
+        return null;
       }
-      return result
+      return result;
     })
-    .then((user) => user ? user.set({ password: body.password }).save() : null)
-    .then((user) => user ? user.view(true) : null)
+    .then(user => (user ? user.set({ password: body.password }).save() : null))
+    .then(user => (user ? user.view(true) : null))
     .then(success(res))
-    .catch(next)
+    .catch(next);
 
 export const destroy = ({ params }, res, next) =>
   User.findById(params.id)
     .then(notFound(res))
-    .then((user) => user ? user.remove() : null)
+    .then(user => (user ? user.remove() : null))
     .then(success(res, 204))
-    .catch(next)
+    .catch(next);
--- src/config.js
+++ src/config.js
@@ -1,35 +1,35 @@
 /* eslint-disable no-unused-vars */
-import path from 'path'
-import merge from 'lodash/merge'
+import path from "path";
+import merge from "lodash/merge";
 
 /* istanbul ignore next */
-const requireProcessEnv = (name) => {
+const requireProcessEnv = name => {
   if (!process.env[name]) {
-    throw new Error('You must set the ' + name + ' environment variable')
+    throw new Error("You must set the " + name + " environment variable");
   }
-  return process.env[name]
-}
+  return process.env[name];
+};
 
 /* istanbul ignore next */
-if (process.env.NODE_ENV !== 'production') {
-  const dotenv = require('dotenv-safe')
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = require("dotenv-safe");
   dotenv.load({
-    path: path.join(__dirname, '../.env'),
-    sample: path.join(__dirname, '../.env.example')
-  })
+    path: path.join(__dirname, "../.env"),
+    sample: path.join(__dirname, "../.env.example")
+  });
 }
 
 const config = {
   all: {
-    env: process.env.NODE_ENV || 'development',
-    root: path.join(__dirname, '..'),
+    env: process.env.NODE_ENV || "development",
+    root: path.join(__dirname, ".."),
     port: process.env.PORT || 9000,
-    ip: process.env.IP || '0.0.0.0',
-    apiRoot: process.env.API_ROOT || '',
-    defaultEmail: 'no-reply@hetea-admin.com',
-    sendgridKey: requireProcessEnv('SENDGRID_KEY'),
-    masterKey: requireProcessEnv('MASTER_KEY'),
-    jwtSecret: requireProcessEnv('JWT_SECRET'),
+    ip: process.env.IP || "0.0.0.0",
+    apiRoot: process.env.API_ROOT || "",
+    defaultEmail: "no-reply@hetea-admin.com",
+    sendgridKey: requireProcessEnv("SENDGRID_KEY"),
+    masterKey: requireProcessEnv("MASTER_KEY"),
+    jwtSecret: requireProcessEnv("JWT_SECRET"),
     mongo: {
       options: {
         db: {
@@ -38,10 +38,10 @@ const config = {
       }
     }
   },
-  test: { },
+  test: {},
   development: {
     mongo: {
-      uri: 'mongodb://localhost/hetea-admin-dev',
+      uri: "mongodb://localhost/hetea-dev",
       options: {
         debug: true
       }
@@ -51,10 +51,10 @@ const config = {
     ip: process.env.IP || undefined,
     port: process.env.PORT || 8080,
     mongo: {
-      uri: process.env.MONGODB_URI || 'mongodb://localhost/hetea-admin'
+      uri: process.env.MONGODB_URI || "mongodb://localhost/hetea-admin"
     }
   }
-}
+};
 
-module.exports = merge(config.all, config[config.all.env])
-export default module.exports
+module.exports = merge(config.all, config[config.all.env]);
+export default module.exports;
