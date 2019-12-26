873d0e2d66933ff3e63a106e6079ac77f8fb4351
Thái Thiên Vũ <thienvu1802@gmail.com>
2019-12-26 08:44:33 +0700

    Add top revenue

--- src/api/statistic/controller.js
+++ src/api/statistic/controller.js
@@ -66,3 +66,56 @@ export const revenue = ({ body }, res, next) => {
     })
     .then(success(res));
 };
+
+export const topTutorRevenue = ({ body }, res, next) => {
+  const startDate = Date.parse(body.startDate);
+  const endDate = Date.parse(body.endDate);
+
+  Contract.aggregate([
+    {
+      $match: {
+        $and: [
+          { status: "done" },
+          {
+            paidDate: {
+              $gte: new Date(startDate),
+              $lte: new Date(endDate)
+            }
+          }
+        ]
+      }
+    },
+    {
+      $project: {
+        price: 1,
+        paidDate: 1,
+        tutor: 1
+      }
+    },
+    {
+      $group: {
+        _id: "$tutor",
+        value: { $sum: "$price" }
+      }
+    },
+    {
+      $sort: {
+        value: 1
+      }
+    }
+  ])
+    .limit(10)
+    .exec()
+    .then(data => {
+      if (data && data.length != 0) {
+        return data.map(record => {
+          return {
+            id: record._id,
+            value: record.value
+          };
+        });
+      }
+      return [];
+    })
+    .then(success(res));
+};
--- src/api/statistic/index.js
+++ src/api/statistic/index.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { middleware as query } from "querymen";
 import { middleware as body } from "bodymen";
 import { password, master, token } from "../../services/passport";
-import { revenue } from "./controller";
+import { revenue, topTutorRevenue } from "./controller";
 
 const router = new Router();
 
@@ -20,5 +20,24 @@ const router = new Router();
  * @apiError 401 Admin access only.
  */
 router.get("/revenue", token({ required: true, roles: ["admin"] }), revenue);
+
+/**
+ * @api {get} /statistics/toptutorrevenue Retrieve top tutor's revenue
+ * @apiName RetrieveTopTutorRevenue
+ * @apiGroup Statistic
+ * @apiPermission admin
+ * @apiParam {String} access_token Admin access_token.
+ * @apiParam {Date} startDate Start date.
+ * @apiParam {Date} endDate End date.
+ * @apiParam {String=day,week,month,year} [unit=day] Unit.
+ * @apiSuccess {Object[]} data Revenue per unit.
+ * @apiError {Object} 400 Some parameters may contain invalid values.
+ * @apiError 401 Admin access only.
+ */
+router.get(
+  "/toptutorrevenue",
+  token({ required: true, roles: ["admin"] }),
+  topTutorRevenue
+);
 
 export default router;
